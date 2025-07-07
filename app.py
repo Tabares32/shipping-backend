8ba14727b5c07aa45d6514b990c928f100629266
Tabares32 <christian@seatcoversunlimited.com>
2025-07-07 12:37:48 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-from routes import shipping_bp
-from models import init_db
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(shipping_bp, url_prefix="/api")
+@app.route("/api/test")
+def test():
+    return jsonify({"message": "Backend conectado correctamente!"})
 
 if __name__ == "__main__":
-    init_db()
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
