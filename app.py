c64ba87f88c3715aeac8f11cc3a3cc43bd4faafe
Tabares32 <christian@seatcoversunlimited.com>
2025-07-07 16:00:16 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/test")
+@app.route(shipping-frontend-ul8f.vercel.app)
 def test():
     return jsonify({"message": "Backend conectado correctamente!"})
 
