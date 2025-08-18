57c8b563c76f1cefeb5c34688e01ba4e1fae7cb7
Tabares32 <christian@seatcoversunlimited.com>
2025-08-18 15:24:50 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -398,5 +398,5 @@ def reports():
 # Main (para local). En Render usa gunicorn.
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5432))
     app.run(host="0.0.0.0", port=port, debug=True)
